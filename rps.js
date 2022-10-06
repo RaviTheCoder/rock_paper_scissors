52c39682af034d97f32c10e494f726ba69a9a76a
RaviTheCoder <rrravitesh@gmail.com>
2022-10-06 16:46:23 +1300

    Intialise getComputerChoice()

--- rps.js
+++ rps.js
@@ -1,1 +1,28 @@
 console.log("Hello World");
+
+// getComputerChoice() will return either r, p or s
+
+function getComputerChoice() {
+    num = Math.floor(Math.random() * 3) + 1;
+    if (num == 1) {
+        result = "rock";
+    } else if (num == 2) {
+        result = "paper";
+    } else {
+        result = "scissors";
+    }
+    return result;
+}
+
+console.log(getComputerChoice());
+
+// playRound(playerSelection, computerSelection) 
+// using if else - find out who wins
+// returns a string declaring round winner
+
+// get player choice
+// make player choice case insensitive
+
+// game() - plays five rounds
+// keeps score
+// declares overall winner
