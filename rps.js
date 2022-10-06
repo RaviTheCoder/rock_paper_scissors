b10600ed3e750405f80efcd843895d3349dc24b4
RaviTheCoder <rrravitesh@gmail.com>
2022-10-06 17:01:12 +1300

    Initialise playRound()

--- rps.js
+++ rps.js
@@ -14,14 +14,56 @@ function getComputerChoice() {
     return result;
 }
 
-console.log(getComputerChoice());
+// get player choice
+// make player choice case insensitive
+
+let playerChoice = "rock";
 
 // playRound(playerSelection, computerSelection) 
 // using if else - find out who wins
 // returns a string declaring round winner
 
-// get player choice
-// make player choice case insensitive
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            result = "draw";
+        } else if (computerSelection == "paper") {
+            result = "player lose";
+        } else { 
+            result = "player win";
+        }
+        console.log(`Player chose ${playerSelection}`);
+        console.log(`Computer chose ${computerSelection}`);
+        // console.log(result);
+        return result;
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            result = "player win";
+        } else if (computerSelection == "paper") {
+            result = "draw";
+        } else { 
+            result = "player lose";
+        }
+        console.log(`Player chose ${playerSelection}`);
+        console.log(`Computer chose ${computerSelection}`);
+        // console.log(result);
+        return result;
+    } else {
+        if (computerSelection == "rock") {
+            result = "player lose";
+        } else if (computerSelection == "paper") {
+            result = "player win";
+        } else { 
+            result = "draw";
+        }
+        console.log(`Player chose ${playerSelection}`);
+        console.log(`Computer chose ${computerSelection}`);
+        // console.log(result);
+        return result;
+    }
+}
+
+console.log(playRound(playerChoice, getComputerChoice()));
 
 // game() - plays five rounds
 // keeps score
