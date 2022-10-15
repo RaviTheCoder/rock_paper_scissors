1088207d6a087299be022b38f4129198d04ff89d
Ravi Ratnam <raviratnam@outlook.com>
2022-10-15 19:05:46 +1300

    Able to declare winner

--- rps.js
+++ rps.js
@@ -54,15 +54,15 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "rock") {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
             ties += 1;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
         } else if (computerSelection == "paper") {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
             robotScore += 1;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
         } else { 
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
             humanScore += 1;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
         }
 
     } else if (playerSelection == "paper") {
@@ -70,144 +70,46 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "rock") {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
             humanScore += 1;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
         } else if (computerSelection == "paper") {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
             ties += 1;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
         } else {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
             robotScore += 1;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
         }
 
     } else {
 
         if (computerSelection == "rock") {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
+            robotScore += 1;
         } else if (computerSelection == "paper") {
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
+            humanScore += 1;
         } else { 
             comment.innerText = `Player ${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()} Computer`;
-            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`
+            scorecard.innerText = `Player ${humanScore} : ${robotScore} Computer and ${ties} draws`;
+            ties =+ 1;
         }
     }
+
+    checkWinner();
 }
 
+// display result on page
 
+let winner = document.createElement('p');
+resultDiv.appendChild(winner);
 
-
-
-
-
-
-
-
-// playRound(playerSelection, computerSelection) 
-// using nested if else - find out who wins
-// returns a string declaring round winner
-
-// function playRound(playerSelection, computerSelection) {
-
-//     if (playerSelection == "rock") {
-//         if (computerSelection == "rock") {
-//             result = "      A draw? boring      ";
-//         } else if (computerSelection == "paper") {
-//             result = "      NOOOO :(       ";
-//         } else { 
-//             result = "      YAAAY! :D       ";
-//         }
-
-//         status.innerText = `Player ${playerSelection.toUpperCase()} : ${computerSelection.toUpperCase()} Computer`;
-
-//         // console.log(`Player ${playerSelection.toUpperCase()} : ${computerSelection.toUpperCase()} Computer`);
-//         return result;
-//     } else if (playerSelection == "paper") {
-//         if (computerSelection == "rock") {
-//             result = "      YAAAY! :D       ";
-//         } else if (computerSelection == "paper") {
-//             result = "      A draw? boring      ";
-//         } else { 
-//             result = "      NOOOO :(       ";
-//         }
-
-//         status.innerText = `Player ${playerSelection.toUpperCase()} : ${computerSelection.toUpperCase()} Computer`;
-
-//         // console.log(`Player ${playerSelection.toUpperCase()} : ${computerSelection.toUpperCase()} Computer`);
-//         return result;
-//     } else {
-//         if (computerSelection == "rock") {
-//             result = "      NOOOO :(       ";
-//         } else if (computerSelection == "paper") {
-//             result = "      YAAAY! :D       ";
-//         } else { 
-//             result = "      A draw? boring      ";
-//         }
-        
-//         status.innerText = `Player ${playerSelection.toUpperCase()} : ${computerSelection.toUpperCase()} Computer`;
-
-//         // console.log(`Player ${playerSelection.toUpperCase()} : ${computerSelection.toUpperCase()} Computer`);
-//         return result;
-//     }
-// }
-
-
-// get player choice
-// make player choice case insensitive
-
-// let getPlayerChoice = () => prompt("Rock, Paper or Scissors?").toLowerCase();
-
-// validate player choice
-// take getPlayerChoice - test against correct selections
-// if correct, send to game()
-// if incorrect, send another prompt
-
-// function validatePlayerChoice(playerSelection) {
-//     if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-//         alert("Please type in rock, paper, or scissors with correct spelling.")
-//         return validatePlayerChoice(getPlayerChoice());
-//     } else {
-//         return playerSelection;
-//     }
-
-// }
-
-// game() - plays five rounds
-// keeps scorefor (let i = 0; i < 5; i++) 
-// declares overall winner
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let draws = 0;
-
-    for (let i = 0; i < 5; i++) {
-        console.log(`      ---  ROUND ${i + 1}  ---`);
-        result = playRound(validatePlayerChoice(getPlayerChoice()), getComputerChoice());
-
-        if (result == "      YAAAY! :D       ") {
-            playerScore += 1;
-        } else if (result == "      NOOOO :(       ") {
-            computerScore += 1;
-        } else {
-            draws += 1;
-        }
-
-        console.log(result);
-        console.log(`Player ${playerScore} : ${computerScore} Computer   and   ${draws} draws`)
-    }
-
-    if (playerScore == computerScore) {
-        console.log("It was a draw!?!?");
-    } else if (playerScore > computerScore) {
-        console.log("Humanity prevails!");
-    } else {
-        console.log("Frick - the robots won :/");
+function checkWinner() {
+    if (humanScore == 5) {
+        winner.innerText = "yay you go human";
+    } else if (robotScore == 5) {
+        winner.innerText = "nooo poor humanity";
     }
 }
-
-// game();
-
-
